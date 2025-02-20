3e698bcb111c10f522b6c0f7818c2d36f54073c3
diosamuel <koding12sam@gmail.com>
2025-02-21 01:00:06 +0700

    feat: up

--- src/routes/tools/instagram-mentions/+page.svelte
+++ src/routes/tools/instagram-mentions/+page.svelte
@@ -1,3 +1,3 @@
-<div class="flex flex-col items-center justify-center text-center bg-black">
-  <h1 class="text-4xl text-white">COOOOOOOMING SOON</h1>
+<div class="flex flex-col items-center justify-center text-center bg-black h-screen">
+  <h1 class="text-4xl text-white">!</h1>
 </div>
